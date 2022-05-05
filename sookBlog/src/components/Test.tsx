f6f3e0d238030ab737edde62ea963481b1f7af3c
엔코 이숙영 <sookyoung.lee@enkor.kr>
2022-05-05 19:26:12 +0900

    Edit README.md

--- sookBlog/src/components/Test.tsx
+++ sookBlog/src/components/Test.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import UsingSSR from "@src/pages/page-2";
+import UsingTypescript from "@src/pages/using-typescript";
 
 const Test = () => {
 	return (
 		<>
+			<UsingSSR />
+			<UsingTypescript />
 			<h1 className="btn-primary ">Gatsby test!</h1>
 			<div className="text-[38px] text-teal-400">Test</div>
 		</>
--- sookBlog/src/pages/index.tsx
+++ sookBlog/src/pages/index.tsx
@@ -1,17 +1,10 @@
-// import Test from "../components/Test";
-import Header from "@src/components/header";
-import Layout from "@src/components/layout";
-import Seo from "@src/components/seo";
-import Test from "@src/components/Test";
 import React from "react";
+import Test from "@src/components/Test";
 
 const index = () => {
 	return (
 		<>
-			<Layout>
-				<Header />
-				<Test />
-			</Layout>
+			<Test />
 		</>
 	);
 };
