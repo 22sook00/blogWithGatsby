04bbd9d63f77175d84698ce4f2d9347d04aff248
Lee Sookyoung <80618616+22sook00@users.noreply.github.com>
2022-05-08 14:52:16 +0900

    Install query-string due to url parsing easily (#5)
    * Add fixed nav & styling on layout background
    
    * Add postlist layout
    
    * Delete unusable directory used before
    
    * Install query-string due to url parsing easily
    
    Co-authored-by: 엔코 이숙영 <sookyoung.lee@enkor.kr>

--- SookDevLog/src/pages/index.tsx
+++ SookDevLog/src/pages/index.tsx
@@ -8,19 +8,33 @@ import CategoryList from "@src/components/main/CategoryList/CategoryList";
 import PostList from "@src/components/main/PostList/PostList";
 import Header from "@src/components/common/Header";
 import Footer from "@src/components/common/Footer";
+import queryString, { ParsedQuery } from "query-string";
 
 const IndexPage = ({
+	location: { search },
 	data: {
 		allMarkdownRemark: { edges },
+		file: {
+			childImageSharp: { gatsbyImageData },
+		},
 	},
 }) => {
+	const parsed: ParsedQuery<string> = queryString.parse(search);
+	console.log("parsed?", parsed);
+	const selectedCategory: string =
+		typeof parsed.category !== "string" || !parsed.category
+			? "All"
+			: parsed.category;
 	return (
 		<main>
 			<Header />
 			<LayoutDefault>
 				<Link to="/info/">To Info</Link>
 				<Introduction />
-				{/* <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} /> */}
+				{/* <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={CATEGORY_LIST}
+      /> */}
 				<PostList edges={edges} />
 			</LayoutDefault>
 			<Footer />
