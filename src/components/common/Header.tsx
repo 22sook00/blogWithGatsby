7d40ca91e2973c618167c5bf270e8262b61243b0
22sook00 <22sook00@gmail.com>
2022-08-15 13:39:45 +0900

    Delete unusable code

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "gatsby";
-import React, { FC, useCallback, useState } from "react";
+import React, { FC } from "react";
 import { ISearchKeywords } from "@src/interface/Ilayout";
 
 const Header: FC<ISearchKeywords> = ({
