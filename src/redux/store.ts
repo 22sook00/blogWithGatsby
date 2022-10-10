e446456f21f29463c59e38f40fba08fc29f85d93
22sook00 <22sook00@gmail.com>
2022-10-10 17:08:12 +0900

    Delete latest post

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,11 +3,9 @@ import postReducer from "@src/redux/slice/postSlice";
 
 export const store = configureStore({
 	reducer: {
-		//all difference slices in here!
 		posts: postReducer,
 	},
 });
 
-//javascript 가 아닌, typescript 에서 필요한것!
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
